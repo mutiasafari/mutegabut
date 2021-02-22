c59e8d20b7489063d2285c42b5113f857eac9ac1
Mutia Safari <mutiasafari97@gmail.com>
2021-02-23 00:21:24 +0700

    add home screen

---
+++ src/screens/Detail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Detail = () => {
+  return (
+    <View
+      style={{
+        justifyContent: 'center',
+        alignContent: 'center',
+        flex: 1,
+      }}>
+      <Text>ini Detail js loh yaa</Text>
+    </View>
+  );
+};
+
+export default Detail;
---
+++ src/screens/Favourite.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Favourite = () => {
+  return (
+    <View
+      style={{
+        justifyContent: 'center',
+        alignContent: 'center',
+        flex: 1,
+      }}>
+      <Text>ini Favourite js loh yaa</Text>
+    </View>
+  );
+};
+
+export default Favourite;
